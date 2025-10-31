0ec54d03ffe62566bc817d1366701478450c5239
Doae Chemchaoui <chemchaouidoae@gmail.com>
2025-10-31 17:01:06 +0100

    Modifier la partie hours

--- src/script.js
+++ src/script.js
@@ -11,11 +11,11 @@ const dateTimer = document.getElementById('dateTimer');
 
 setInterval(() => {
   const d = new Date();
-  dateTimer.textContent = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} - ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+  dateTimer.textContent = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
 }, 1000);
 
 
-const hours = [
+const hours = ["08:00",
   "09:00","10:00","11:00","12:00","13:00","14:00",
   "15:00","16:00","17:00","18:00","19:00","20:00",
   "21:00","22:00","23:00","00:00"
